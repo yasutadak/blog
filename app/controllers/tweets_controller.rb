c53b4a0cf0fd391b1b7880450266d3ac56b3fa4c
yasutadak <yasutada629@gmail.com>
2019-01-10 19:43:00 +0900

    Textの投稿機能の作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,5 +2,21 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all
+
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to action: :index
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'tweets' => 'tweets#index'
+  get  'tweets'      => 'tweets#index'
+  get  'tweets/new'  => 'tweets#new'
+  post 'tweets'      => 'tweets#create'
 end
